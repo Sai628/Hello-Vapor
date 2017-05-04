66d58e171e0a6afc5ace227cbbe07d98d4ed5240
Sai628 <sai100628@gmail.com>
2017-05-04 21:45:01 +0800

    add test api

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,6 +1,8 @@
 import Vapor
 
+
 let drop = Droplet()
+
 
 drop.get { req in
     return try drop.view.make("welcome", [
@@ -8,13 +10,27 @@ drop.get { req in
     ])
 }
 
+
 drop.get("/hello") { _ in
     return "Hello Vapor"
 }
 
+
+drop.get("/name", ":name") { request in
+    
+    if let name = request.parameters["name"]?.string
+    {
+        return "Hello \(name)"
+    }
+    
+    return "Error retrieving parameters."
+}
+
+
 drop.get("/test") { request in
     return "\(request)"
 }
+
 
 drop.resource("posts", PostController())
 
